12d1a8e87118d72324a9bf2b55dc2d5645e46176
anupm <anupmehta702@gmail.com>
2020-05-07 13:31:33 +0530

    Adding bucket fill functionality and tests

--- src/main/java/com/anup/paint/canvas/operation/BucketFill.java
+++ src/main/java/com/anup/paint/canvas/operation/BucketFill.java
@@ -1,11 +1,30 @@
 package com.anup.paint.canvas.operation;
 
 import com.anup.paint.canvas.Canvas;
+import com.anup.paint.command.model.Coordinates;
 import com.anup.paint.command.model.InputCommand;
 
 public class BucketFill implements CanvasOperation {
     @Override
     public void execute(InputCommand input, Canvas canvas) {
+        Character[][] clonedDrawingArea = canvas.getClonedDrawingArea();
+        Coordinates start = input.getStart();
+        fillColor(start.getX(), start.getY(), clonedDrawingArea, input.getMarker());
+        canvas.mergeDrawingChanges(clonedDrawingArea);
+    }
+
+    public void fillColor(int x, int y, Character[][] clonedDrawingArea, Character marker) {
+        if (x > clonedDrawingArea.length - 2 || y > clonedDrawingArea[0].length - 2)
+            return;
+        if (x <= 0 || y <= 0)
+            return;
+        if (clonedDrawingArea[x][y] != ' ')
+            return;
+        clonedDrawingArea[x][y] = marker;
+        fillColor(x + 1, y, clonedDrawingArea, marker);//bottom
+        fillColor(x - 1, y, clonedDrawingArea, marker);//top
+        fillColor(x, y + 1, clonedDrawingArea, marker);//right
+        fillColor(x, y - 1, clonedDrawingArea, marker);//left
 
     }
 }
--- src/test/java/com/anup/paint/canvas/operation/BucketFillTest.java
+++ src/test/java/com/anup/paint/canvas/operation/BucketFillTest.java
@@ -1,45 +1,72 @@
 package com.anup.paint.canvas.operation;
 
 import com.anup.paint.canvas.Canvas;
-import com.anup.paint.command.exception.OutOfBoundaryBaseException;
+import com.anup.paint.canvas.exception.CanvasOperationBaseException;
 import com.anup.paint.command.model.CommandType;
 import com.anup.paint.command.model.Coordinates;
 import com.anup.paint.command.model.InputCommand;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
 
 public class BucketFillTest {
 
-    Canvas inputCanvas = new Canvas(7, 7);
-    BucketFill bucketFill = new BucketFill();
-    Character[][] expectedDrawingArea = inputCanvas.getClonedDrawingArea();
+    Canvas inputCanvas ;
+    BucketFill bucketFill ;
+    Character[][] expectedDrawingArea ;
+    InputCommand input ;
+
+    @Before
+    public void setup() {
+        inputCanvas = new Canvas(7, 7);
+        bucketFill = new BucketFill();
+        expectedDrawingArea = inputCanvas.getClonedDrawingArea();
+        input = new InputCommand(CommandType.BUCKET_FILL, new Coordinates(7, 7), 'c');
+    }
 
 
     @Test
-    public void executeTest() throws OutOfBoundaryBaseException {
+    public void executeTest() throws  CanvasOperationBaseException {
         Rectangle rectangle = new Rectangle();
         InputCommand rectInput = new InputCommand(CommandType.RECTANGLE,
-                new Coordinates(2, 2), new Coordinates(5, 5));
+                new Coordinates(2, 2), new Coordinates(4, 4));
         rectangle.execute(rectInput, inputCanvas);
-        expectedDrawingArea = inputCanvas.getClonedDrawingArea();
-        bucketFill.execute(new InputCommand(CommandType.BUCKET_FILL, new Coordinates(6, 2), 'c'), inputCanvas);
 
-        System.out.println("-->");
-        for (int i = 1; i <= 6; i++) {
-            for (int j = expectedDrawingArea[0].length - 2; j >= 1; j--) {
-                if(expectedDrawingArea[i][j] == 'x'){
-                    do{
-                        j--;
-                    }while((expectedDrawingArea[i][j] != 'x'));
-                }else{
-                    expectedDrawingArea[i][j] = 'c';
-                }
+        Line line = new Line();
+        InputCommand hLine = new InputCommand(CommandType.LINE,
+                new Coordinates(6, 1), new Coordinates(6, 2));
+        line.execute(hLine,inputCanvas);
+
+
+        InputCommand vLine = new InputCommand(CommandType.LINE,
+                new Coordinates(6, 2), new Coordinates(7, 2));
+        line.execute(vLine,inputCanvas);
+
+        InputCommand hLine2 = new InputCommand(CommandType.LINE,
+                new Coordinates(2, 6), new Coordinates(2, 7));
+        line.execute(hLine2,inputCanvas);
+
+        InputCommand vLine2 = new InputCommand(CommandType.LINE,
+                new Coordinates(1, 6), new Coordinates(2, 6));
+        line.execute(vLine2,inputCanvas);
+
+        expectedDrawingArea = inputCanvas.getClonedDrawingArea();
+        bucketFill.execute(input, inputCanvas);
+
+        for (int i = 1; i <= expectedDrawingArea.length-2; i++) {
+            for (int j =1 ; j <= expectedDrawingArea[0].length-2 ;j++) {
+                 if(expectedDrawingArea[i][j] == 'x' || (i==3&&j==3) || (i==1 && j==7) || (i==7 && j==1)) {
+                     //do nothing
+                 }else{
+                     expectedDrawingArea[i][j]='c';
+                 }
             }
         }
         inputCanvas.mergeDrawingChanges(expectedDrawingArea);
         inputCanvas.print();
 
-
-        //assertArrayEquals(expectedDrawingArea, inputCanvas.getClonedDrawingArea());
+       assertArrayEquals(expectedDrawingArea, inputCanvas.getClonedDrawingArea());
     }
 
 }
